73e7380a10d2af511e268e5b8700d0d8c7ba6c80
levybooth <levybooth@users.noreply.github.com>
2015-08-25 15:38:39 -0700

    Update ngenesPCA.R

--- ngenesPCA.R
+++ ngenesPCA.R
@@ -2,31 +2,31 @@
 library(vegan)
 library(ggplot2)
 
-# nifH
-nifh_nh <- as.matrix(rnorm(3, mean=2.32e7, sd=0.02e7))
-nifh_yl <- as.matrix(rnorm(3, mean=4.36e7, sd=0.10e7))
-nifh_zl <- as.matrix(rnorm(3, mean=1.37e8, sd=0.06e8))
-nifh_cl <- as.matrix(rnorm(3, mean=1.89e8, sd=0.05e8))
+# nifH 
+nifh_nh <- as.matrix(rnorm(3, mean=2.32e7, sd=0.02e7 * sqrt(3)))
+nifh_yl <- as.matrix(rnorm(3, mean=4.36e7, sd=0.10e7 * sqrt(3)))
+nifh_zl <- as.matrix(rnorm(3, mean=1.37e8, sd=0.06e8 * sqrt(3)))
+nifh_cl <- as.matrix(rnorm(3, mean=1.89e8, sd=0.05e8 * sqrt(3)))
 # AOB
-aob_nh <- as.matrix(rnorm(3, mean=1.90e6, sd=0.05e6))
-aob_yl <- as.matrix(rnorm(3, mean=4.58e6, sd=0.03e6))
-aob_zl <- as.matrix(rnorm(3, mean=8.24e6, sd=0.04e6))
-aob_cl <- as.matrix(rnorm(3, mean=6.13e6, sd=0.09e6))
+aob_nh <- as.matrix(rnorm(3, mean=1.90e6, sd=0.05e6 * sqrt(3)))
+aob_yl <- as.matrix(rnorm(3, mean=4.58e6, sd=0.03e6 * sqrt(3)))
+aob_zl <- as.matrix(rnorm(3, mean=8.24e6, sd=0.04e6 * sqrt(3)))
+aob_cl <- as.matrix(rnorm(3, mean=6.13e6, sd=0.09e6 * sqrt(3)))
 # AOA
-aoa_nh <- as.matrix(rnorm(3, mean=1.40e3, sd=0.01e3))
-aoa_yl <- as.matrix(rnorm(3, mean=2.16e3, sd=0.02e3))
-aoa_zl <- as.matrix(rnorm(3, mean=7.15e3, sd=0.01e3))
-aoa_cl <- as.matrix(rnorm(3, mean=4.12e3, sd=0.03e3))
+aoa_nh <- as.matrix(rnorm(3, mean=1.40e3, sd=0.01e3 * sqrt(3)))
+aoa_yl <- as.matrix(rnorm(3, mean=2.16e3, sd=0.02e3 * sqrt(3)))
+aoa_zl <- as.matrix(rnorm(3, mean=7.15e3, sd=0.01e3 * sqrt(3)))
+aoa_cl <- as.matrix(rnorm(3, mean=4.12e3, sd=0.03e3 * sqrt(3)))
 # nirK
-nirk_nh <- as.matrix(rnorm(3, mean=3.56e5, sd=0.10e5))
-nirk_yl <- as.matrix(rnorm(3, mean=7.45e5, sd=0.02e5))
-nirk_zl <- as.matrix(rnorm(3, mean=1.39e6, sd=0.04e6))
-nirk_cl <- as.matrix(rnorm(3, mean=2.48e6, sd=0.05e6))
+nirk_nh <- as.matrix(rnorm(3, mean=3.56e5, sd=0.10e5 * sqrt(3)))
+nirk_yl <- as.matrix(rnorm(3, mean=7.45e5, sd=0.02e5 * sqrt(3)))
+nirk_zl <- as.matrix(rnorm(3, mean=1.39e6, sd=0.04e6 * sqrt(3)))
+nirk_cl <- as.matrix(rnorm(3, mean=2.48e6, sd=0.05e6 * sqrt(3)))
 # narG
-narg_nh <- as.matrix(rnorm(3, mean=1.59e6, sd=0.02e6))
-narg_yl <- as.matrix(rnorm(3, mean=5.37e6, sd=0.06e6))
-narg_zl <- as.matrix(rnorm(3, mean=7.17e6, sd=0.04e6))
-narg_cl <- as.matrix(rnorm(3, mean=1.60e7, sd=0.04e7))
+narg_nh <- as.matrix(rnorm(3, mean=1.59e6, sd=0.02e6 * sqrt(3)))
+narg_yl <- as.matrix(rnorm(3, mean=5.37e6, sd=0.06e6 * sqrt(3)))
+narg_zl <- as.matrix(rnorm(3, mean=7.17e6, sd=0.04e6 * sqrt(3)))
+narg_cl <- as.matrix(rnorm(3, mean=1.60e7, sd=0.04e7 * sqrt(3)))
 
 #Combine data to dataframe
 nifh <- rbind(nifh_nh, nifh_yl, nifh_zl, nifh_cl)
@@ -44,7 +44,7 @@ ngenes_pca <- princomp(ngenes[-6], cor=TRUE)
 #format scores for PCA plot
 var_x <- ngenes_pca$sdev[1]^2/sum(ngenes_pca$sdev[1]^2, ngenes_pca$sdev[2]^2, ngenes_pca$sdev[3]^2, ngenes_pca$sdev[4]^2, ngenes_pca$sdev[5]^2)
 var_y <- ngenes_pca$sdev[2]^2/sum(ngenes_pca$sdev[1]^2, ngenes_pca$sdev[2]^2, ngenes_pca$sdev[3]^2, ngenes_pca$sdev[4]^2, ngenes_pca$sdev[5]^2)
-
+                                                                    
 ngenes_x <- as.data.frame(ngenes_pca$scores[,1])
 ngenes_y <- as.data.frame(ngenes_pca$scores[,2])
 site <- ngenes["site"]
@@ -71,3 +71,10 @@ summary(aoa_anova); TukeyHSD(aoa_anova)
 summary(aob_anova); TukeyHSD(aob_anova)
 summary(nirk_anova); TukeyHSD(nirk_anova)
 summary(narg_anova); TukeyHSD(narg_anova)
+
+par(mfrow = c(1, 5))
+boxplot(nifh ~ site, data = ngenes, main = "nifH")
+boxplot(aoa ~ site, data = ngenes, main = "AOA")
+boxplot(aob ~ site, data = ngenes, main = "AOB")
+boxplot(nirk ~ site, data = ngenes, main = "nirK")
+boxplot(narg ~ site, data = ngenes, main = "narG")
